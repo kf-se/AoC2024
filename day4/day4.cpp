f82f9d807530a6039bcc5244e5e184c1efca6208
kfse <sidllub@gmail.com>
2025-07-02 20:13:39 +0200

    looking for xmas, samx horisontally

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,23 +1,80 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <string>
 #include <vector>
 
-std::stringstream LOG;
+using namespace std;
+stringstream LOG;
 
-std::vector<int> getData(std::filesystem::path fp) {
-  std::vector<int> ret;
-  std::ifstream istrm(fp);
+vector<string> getData(filesystem::path fp) {
+  vector<string> ret;
+  ifstream istrm(fp);
 
   if (!istrm.is_open()) {
-    LOG << "Failed to open file: " << fp.c_str() << std::endl;
+    LOG << "Failed to open file: " << fp.c_str() << endl;
+  }
+
+  string line;
+  while (getline(istrm, line)) {
+    ret.push_back(line);
   }
 
   return ret;
 }
 
+int findXmas(const vector<string> &data) {
+  const map<int, char> horizontal = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+  const map<int, char> vertical = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+  const map<int, char> diagonalLeft = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+  const map<int, char> diagonalRight = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+
+  int rx = 0;
+  int nrXmas = 0;
+
+  while (rx < data.size()) {
+    int cx = 0;
+    string row = data.at(rx);
+    size_t rowSize = row.size();
+    while (cx < rowSize) {
+      cout << rx << " " << cx << endl;
+      if (row.at(cx) == 'X') {
+        // Find horizontal
+        {
+          bool leftMatch = true;
+          bool rightMatch = true;
+          for (const auto &[k, v] : horizontal) {
+            int left = -k;
+            int right = k;
+            cout << cx + right << endl;
+            if (cx + left < 0 || row.at(cx + left) != v)
+              leftMatch = false;
+            if (cx + right >= rowSize || row.at(cx + right) != v)
+              rightMatch = false;
+          }
+          if (leftMatch)
+            ++nrXmas;
+          if (rightMatch)
+            ++nrXmas;
+        }
+      }
+      ++cx;
+    }
+    ++rx;
+  }
+
+  return nrXmas;
+}
+
 int main() {
   auto data = getData("day4-input.txt.out");
   LOG << data.size();
-  std::cout << LOG.str() << std::endl;
+  cout << LOG.str() << endl;
+  cout << data[0].size() << endl;
+  for (int c = 0; c < 3; c++) {
+    cout << data[c] << endl;
+  }
+  int ret = findXmas(data);
+  cout << "answer: " << ret << endl;
 }
--- utilsCmdLine.cpp
+++ utilsCmdLine.cpp
@@ -1,22 +1,21 @@
 #include <algorithm>
 #include <iostream>
+#include <unordered_map>
 
 #include "helpers.h"
 
 enum class State { idle, chdf, quit };
 
-void printVector(const std::vector<std::string>& vec) {
+void printVector(const std::vector<std::string> &vec) {
   std::for_each(vec.begin(), vec.end(),
-                [](const std::string& s) { std::cout << "'" << s << "',"; });
+                [](const std::string &s) { std::cout << "'" << s << "',"; });
   std::cout << std::endl;
 }
 
-void print(const std::string s) {
-  std::cout << s << std::endl;
-}
+void print(const std::string s) { std::cout << s << std::endl; }
 
-State parseCommand(const std::unordered_map<std::string, State>& cmdsMap,
-                   const std::vector<std::string>& args, State state) {
+State parseCommand(const std::unordered_map<std::string, State> &cmdsMap,
+                   const std::vector<std::string> &args, State state) {
   for (auto argc : args) {
     auto it = cmdsMap.find(argc);
     if (it != cmdsMap.end()) {
@@ -27,7 +26,7 @@ State parseCommand(const std::unordered_map<std::string, State>& cmdsMap,
   return state;
 }
 
-void executeCmdHelp(const State& state) {
+void executeCmdHelp(const State &state) {
   if (state == State::chdf) {
     print("Change delimiter of file\n");
     print("filename delimiter_in delimiter_out\n");
@@ -37,7 +36,7 @@ void executeCmdHelp(const State& state) {
 static const std::unordered_map<std::string, State> cmdsMap = {
     {"chdf", State::chdf}, {"quit", State::quit}, {"exit", State::quit}};
 
-static void executeChdf(const std::vector<std::string>& args) {
+static void executeChdf(const std::vector<std::string> &args) {
   if (args.size() < 3) {
     print("Not enough arguments.");
     return;
@@ -48,7 +47,7 @@ static void executeChdf(const std::vector<std::string>& args) {
   changeDelimiterOfFile(fp, fp_out, args.at(1), args.at(2));
 }
 
-std::vector<std::string> tokenize(const std::string& line) {
+std::vector<std::string> tokenize(const std::string &line) {
   std::istringstream iss(line);
   std::vector<std::string> tokens;
   std::string token;
@@ -60,7 +59,7 @@ std::vector<std::string> tokenize(const std::string& line) {
   return tokens;
 }
 
-int main(int argc, char** /* argv */) {
+int main(int argc, char ** /* argv */) {
   print("Utilities for parsing/manipulating raw AoC input data\n");
   print("Type 'exit/quit' to quit.\n");
 
@@ -73,19 +72,20 @@ int main(int argc, char** /* argv */) {
 
     auto args = tokenize(line);
 
-    if (args.empty()) continue;
+    if (args.empty())
+      continue;
     printVector(args);
     auto next_state = parseCommand(cmdsMap, args, state);
 
     switch (state) {
-      case State::idle:
-        executeCmdHelp(next_state);
-        break;
-      case State::chdf:
-        executeChdf(args);
-        break;
-      default:
-        break;
+    case State::idle:
+      executeCmdHelp(next_state);
+      break;
+    case State::chdf:
+      executeChdf(args);
+      break;
+    default:
+      break;
     }
     if (next_state != state) {
       state = next_state;
