af2854d5749f1f0055b2541326ae3555dc571b2c
kfse <sidllub@gmail.com>
2025-07-03 20:35:52 +0200

    pretty close to solving day4, off by 1 on example

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -25,37 +25,78 @@ vector<string> getData(filesystem::path fp) {
 }
 
 int findXmas(const vector<string> &data) {
-  const map<int, char> horizontal = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
-  const map<int, char> vertical = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
-  const map<int, char> diagonalLeft = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
-  const map<int, char> diagonalRight = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
-
+  const map<int, char> xmas = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+  size_t dataSize = data.size();
   int rx = 0;
   int nrXmas = 0;
 
-  while (rx < data.size()) {
+  while (rx < dataSize) {
     int cx = 0;
     string row = data.at(rx);
     size_t rowSize = row.size();
+
     while (cx < rowSize) {
-      cout << rx << " " << cx << endl;
       if (row.at(cx) == 'X') {
         // Find horizontal
         {
           bool leftMatch = true;
           bool rightMatch = true;
-          for (const auto &[k, v] : horizontal) {
-            int left = -k;
-            int right = k;
-            cout << cx + right << endl;
-            if (cx + left < 0 || row.at(cx + left) != v)
+          for (const auto &[k, v] : xmas) {
+            if (cx - k < 0 || row.at(cx - k) != v)
               leftMatch = false;
-            if (cx + right >= rowSize || row.at(cx + right) != v)
+            if (cx + k >= rowSize || row.at(cx + k) != v)
               rightMatch = false;
           }
           if (leftMatch)
             ++nrXmas;
           if (rightMatch)
+            ++nrXmas;
+        }
+        // Find vertical
+        {
+          bool upMatch = true;
+          bool downMatch = true;
+          for (const auto &[k, v] : xmas) {
+            if (rx - k < 0 || data.at(rx - k).at(cx) != v)
+              upMatch = false;
+            if (rx + k >= dataSize || cx >= data.at(rx + k).size() ||
+                data.at(rx + k).at(cx) != v)
+              downMatch = false;
+          }
+          if (upMatch)
+            ++nrXmas;
+          if (downMatch)
+            ++nrXmas;
+        }
+        // Find diagonal
+        {
+          bool upLeftMatch = true;
+          bool upRightMatch = true;
+          bool downLeftMatch = true;
+          bool downRightMatch = true;
+
+          for (const auto &[k, v] : xmas) {
+            if (rx - k < 0 || cx - k < 0 || data.at(rx - k).at(cx - k) != v)
+              upLeftMatch = false;
+            if (rx - k < 0 || cx + k >= data.at(rx - k).size() ||
+                data.at(rx - k).at(cx + k) != v)
+              upRightMatch = false;
+            if (rx + k >= dataSize || cx + k >= data.at(rx + k).size() ||
+                data.at(rx + k).at(cx + k) != v)
+              downRightMatch = false;
+            if (rx + k >= dataSize || cx - k < 0 ||
+                cx + k > data.at(rx + k).size() ||
+                data.at(rx + k).at(cx - k) != v)
+              downLeftMatch = false;
+          }
+
+          if (upLeftMatch)
+            ++nrXmas;
+          if (downLeftMatch)
+            ++nrXmas;
+          if (upRightMatch)
+            ++nrXmas;
+          if (downRightMatch)
             ++nrXmas;
         }
       }
@@ -72,9 +113,8 @@ int main() {
   LOG << data.size();
   cout << LOG.str() << endl;
   cout << data[0].size() << endl;
-  for (int c = 0; c < 3; c++) {
-    cout << data[c] << endl;
-  }
+  data = {"MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM",
+          "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"};
   int ret = findXmas(data);
   cout << "answer: " << ret << endl;
 }
