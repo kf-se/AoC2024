2bbb9da87f23eccb3b61bf490eb8a7bf503ce454
kfse <sidllub@gmail.com>
2025-07-06 12:13:17 +0200

    Another brute force solution, solved day4

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -24,8 +24,59 @@ vector<string> getData(filesystem::path fp) {
   return ret;
 }
 
+int findMas(const vector<string> &data) {
+  size_t dataSize = data.size();
+  int rx = 0;
+  int nrXmas = 0;
+
+  while (rx < dataSize) {
+    int cx = 0;
+    string row = data.at(rx);
+    size_t rowSize = row.size();
+
+    while (cx < rowSize) {
+      if (row.at(cx) == 'A') {
+        bool tl = false;
+        bool bl = false;
+        bool tr = false;
+        bool br = false;
+        int rA = rx + 1;
+        int rB = rx - 1;
+        int cB = cx - 1;
+        int cA = cx + 1;
+        if (rB >= 0 && cB >= 0 && data.at(rB).at(cB) == 'M') {
+          if (rA < dataSize && cA < data.at(rA).size() &&
+              data.at(rA).at(cA) == 'S')
+            tl = true;
+        }
+        if (rB >= 0 && cA < data.at(rB).size() && data.at(rB).at(cA) == 'M') {
+          if (rA < dataSize && cB >= 0 && data.at(rA).at(cB) == 'S')
+            tr = true;
+        }
+        if (rA < dataSize && cA < data.at(rA).size() &&
+            data.at(rA).at(cA) == 'M') {
+          if (rB >= 0 && cB >= 0 && data.at(rB).at(cB) == 'S') {
+            br = true;
+          }
+        }
+        if (rA < dataSize && cB >= 0 && data.at(rA).at(cB) == 'M') {
+          if (rB >= 0 && cA < data.at(rB).size() && data.at(rB).at(cA) == 'S')
+            bl = true;
+        }
+        if ((tl && tr) || (tl && bl) || (tr && br) || (bl && br))
+          ++nrXmas;
+      }
+      ++cx;
+    }
+    ++rx;
+  }
+
+  return nrXmas;
+}
+
 int findXmas(const vector<string> &data) {
   const map<int, char> xmas = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
+  const map<int, char> mas = {{1, 'M'}, {2, 'A'}, {3, 'S'}};
   size_t dataSize = data.size();
   int rx = 0;
   int nrXmas = 0;
@@ -105,6 +156,11 @@ int main() {
   vector<string> data2 = {
       "MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM",
       "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"};
-  int ret = findXmas(data);
-  cout << "answer: " << ret << endl;
+  vector<string> data3 = {
+      ".M.S......", "..A..MSMS.", ".M.S.MAA..", "..A.ASMSM.", ".M.S.M....",
+      "..........", "S.S.S.S.S.", ".A.A.A.A..", "M.M.M.M.M.", ".........."};
+  // int ret = findXmas(data);
+  int ret2 = findMas(data);
+  // cout << "answer: " << ret << endl;
+  cout << "answer2: " << ret2 << endl;
 }
